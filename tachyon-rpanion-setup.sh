b6eb7820c63c9aea4d711c065f6993d72e9a6721
Alejandro <29783316+Pirateparty99@users.noreply.github.com>
2025-08-10 18:47:20 -0500

    Removed old tests from script.

--- tachyon-rpanion-setup.sh
+++ tachyon-rpanion-setup.sh
@@ -54,7 +54,6 @@ npm i || true
 # npm run dev
 
 # Create Debian package for Rpanion Server:
-pwd
 sed -ie 's/${DEB_HOST_ARCH}/arm64/g' ../package.json
 
 # Change dependencies to Ubuntu 20 dependencies:
@@ -65,71 +64,3 @@ npm run package && dpkg -i ../rpanion-server_*_arm64.deb
 
 # Set IP address for eth0 connection to peer to MavLink network (can be changed):
 nmcli connection modify 'Wired connection 1' ipv4.addresses 192.168.144.100/24 ipv4.gateway 192.168.144.1
-
-# === Not needed??? ===
-
-# # Install the required packages:
-# apt install -y gstreamer1.0-libav libopus0 gstreamer1.0-plugins-good libgstrtspserver-1.0-dev gstreamer1.0-plugins-base-apps
-# apt install -y gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad python3-netifaces
-# apt install -y network-manager python3 python3-dev python3-gst-1.0 python3-pip dnsmasq git ninja-build jq
-# apt install -y libxml2-dev libxslt1-dev python3-lxml python3-numpy python3-future gpsbabel zip
-# apt install -y hostapd
-# curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
-# # pip3 install --upgrade --root-user-action kms+ libcamera 
-
-# # If running on RasPiOS, install the libcamera drivers:
-# apt install -y gstreamer1.0-libcamera python3-picamera2 python3-libcamera python3-kms++
-
-
-# # Grab newer versions of Rpanion Server dependencies:
-
-# # https://launchpad.net/ubuntu/+source/gst-rtsp-server1.0/1.20.1-1/+build/23243786/+files/
-
-# cat << EOF > /etc/apt/sources.list.d/ubuntu22.list
-# deb http://ports.ubuntu.com/ubuntu-ports/ jammy main restricted universe multiverse
-# # deb-src http://ports.ubuntu.com/ubuntu-ports/ jammy main restricted universe multiverse
-
-# deb http://ports.ubuntu.com/ubuntu-ports/ jammy-updates main restricted universe multiverse
-# # deb-src http://ports.ubuntu.com/ubuntu-ports/ jammy-updates main restricted universe multiverse
-
-# deb http://ports.ubuntu.com/ubuntu-ports/ jammy-security main restricted universe multiverse
-# # deb-src http://ports.ubuntu.com/ubuntu-ports/ jammy-security main restricted universe multiverse
-
-# deb http://ports.ubuntu.com/ubuntu-ports/ jammy-backports main restricted universe multiverse
-# # deb-src http://ports.ubuntu.com/ubuntu-ports/ jammy-backports main restricted universe multiverse
-# EOF
-
-# # Pin Python before upgrading packages from Jammy repos:
-# cat << EOF > /etc/apt/preferences.d/pin-python
-
-# Package: python3
-# Pin: version 3.8.2-0ubuntu2
-# Pin-Priority: 1001
-
-# Package: python3-dev
-# Pin: version 3.8.2-0ubuntu2
-# Pin-Priority: 1001
-
-# Package: python3-pip
-# Pin: version 20.0.2-5ubuntu1.11
-# Pin-Priority: 1001
-# EOF
-
-# mv /etc/apt/sources.list  /etc/apt/.sources.list
-
-# apt update
-# apt install -y gstreamer1.0-plugins-good gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad network-manager libunwind-dev
-
-# mv /etc/apt/sources.list.d/ubuntu22.list /etc/apt/sources.list.d/.ubuntu22.list
-# mv /etc/apt/.sources.list  /etc/apt/sources.list
-
-# apt update
-# apt install --fix-broken -y
-
-# # Grab Rpanion Server Debian package and install:
-# wget https://github.com/stephendade/Rpanion-server/releases/download/v0.11.4/rpanion-server_0.11.4_arm64.deb
-# dpkg -i rpanion-server_0.11.4_arm64.deb
-
-# apt remove -y update-manager-core
-
-# ===============================================
